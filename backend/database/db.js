d0bc822c54d85a44e072920af70a66e9d159b3f9
juan garcia <juanjose10312@gmail.com>
2024-12-07 14:05:28 -0700

    conexion a databse aws rds

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,8 @@
 // Sequelize es un ORM para Node.js
 import { Sequelize } from "sequelize";
-
-const db = new Sequelize("crud_app", "root", "", {
-  host: "localhost",
+// nombre-db, user, password
+const db = new Sequelize("crud_app", "admin", "rootawsdb", {
+  host: "database-notas.chmu0coycs49.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
 });
 

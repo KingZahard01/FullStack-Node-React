ab1153313991c479560e8616fe1988886653f8bb
juan garcia <juanjose10312@gmail.com>
2025-02-01 20:55:36 -0700

    conection to zeabur db

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,14 +1,31 @@
 // Sequelize es un ORM para Node.js
 import { Sequelize } from "sequelize";
 // nombre-db, user, password
-const db = new Sequelize("crud_app", "admin", "rootawsdb", {
-  host: "database-notas.chmu0coycs49.us-east-1.rds.amazonaws.com",
+const db = new Sequelize("zeabur", "root", "EpcjFvSO426K8LWG1gmD0nNr95Yz7xh3", {
+  host: "free.clusters.zeabur.com",
   dialect: "mysql",
+  port: 32438,
 });
 
 export default db;
 
+/* zeabur: 
+username=root
+password= EpcjFvSO426K8LWG1gmD0nNr95Yz7xh3
+database=zeabur
+host=free.clusters.zeabur.com
+port=32438
+*/
+
 /*
+CREATE TABLE zeabur.blogs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    content VARCHAR(300) NOT NULL,
+    createdAt DATETIME,
+    updatedAt DATETIME
+);
+
 INSERT INTO blogs (id, title, content) VALUES
 (1, 'Título 1', 'Contenido de mi primer post'),
 (2, 'Título 2', 'Contenido de mi segundo post'),
